173dd8a1afed4c774d850e32f2738b5005311bd2
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-02-16 13:50:45 +0200

    adding routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,45 @@
-import { useEffect, useState } from 'react';
-import { getTrendingMovie } from '../apiService/Films';
-import './App.css';
-import { MovieList } from './MovieList';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import clsx from 'clsx';
+// import { useEffect, useState } from 'react';
+// import { getTrendingMovie } from '../apiService/Films';
+// import { MovieList } from './MovieList';
 
 export const App = () => {
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    getTrendingMovie()
-      .then(resp => {
-        setMovies(resp.movies);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   getTrendingMovie()
+  //     .then(resp => {
+  //       setMovies(resp.movies);
+  //       console.log(movies);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
+  // http://localhost:5173/home
+  // http://localhost:5173/movies
 
   return (
     <div>
-      <h2>Trending now</h2>
-      <MovieList movies={movies} />
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) => {
+            return clsx();
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="*" element={<div>Page not found</div>} />
+      </Routes>
     </div>
   );
 };
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,6 +2,7 @@ export const MovieList = ({ movies }) => {
   console.log(movies);
   return (
     <div>
+      <h2>Trending now</h2>
       {movies.length > 0 && (
         <ul>
           {movies.map(movie => {
